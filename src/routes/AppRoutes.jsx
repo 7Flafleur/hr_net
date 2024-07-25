d925c0521454614e47383b0917aacdb50addff57
7Flafleur <7nb@hotmail.fr>
2024-07-25 12:26:53 +0200

    user fetch ok

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateEmployeePage from '../pages/CreateEmployeePage';
 import EmployeeList1 from '../pages/EmployeeList';
-import EmployeeList2 from '../pages/EmployeeList2';
+// import EmployeeList2 from '../pages/EmployeeList2';
 
 
 export const AppRoutes = () => {
@@ -10,7 +10,7 @@ export const AppRoutes = () => {
             <Routes>         
                 <Route path="/" element={<CreateEmployeePage />} />
                 <Route path="/employee-list1" element={<EmployeeList1 />} />
-                <Route path="/employee-list2" element={<EmployeeList2 />} />
+                {/* <Route path="/employee-list2" element={<EmployeeList2 />} /> */}
             </Routes>
         </Router>
     );
--- src/utils/MockUsers.js
+++ src/utils/MockUsers.js
@@ -1,58 +1,58 @@
  export const mockUsers = [
     {
-      firstname: 'TestEmployee1',
-      lastname: 'One',
-      startdate: '01/01/2020',
+      firstName: 'TestEmployee1',
+      lastName: 'One',
+      startDate: '01/01/2020',
       department: 'Human Resources',
       dob: '31/01/2000',
       street: 'Bstreet',
       city: 'Asity',
       state: 'B',
-      zipcode: '02'
+      zipCode: '02'
     },
     {
-      firstname: 'TestEmployee2',
-      lastname: 'Two',
-      startdate: '15/03/2021',
+      firstName: 'TestEmployee2',
+      lastName: 'Two',
+      startDate: '15/03/2021',
       department: 'Sales',
       dob: '12/06/1998',
       street: 'Cstreet',
       city: 'Bcity',
       state: 'C',
-      zipcode: '03'
+      zipCode: '03'
     },
     {
-      firstname: 'TestEmployee3',
-      lastname: 'Three',
-      startdate: '22/07/2019',
+      firstName: 'TestEmployee3',
+      lastName: 'Three',
+      startDate: '22/07/2019',
       department: 'Marketing',
       dob: '23/11/1995',
       street: 'Dstreet',
       city: 'Ccity',
       state: 'D',
-      zipcode: '04'
+      zipCode: '04'
     },
     {
-      firstname: 'TestEmployee4',
-      lastname: 'Four',
-      startdate: '30/08/2018',
+      firstName: 'TestEmployee4',
+      lastName: 'Four',
+      startDate: '30/08/2018',
       department: 'Engineering',
       dob: '14/02/1997',
       street: 'Estreet',
       city: 'Dcity',
       state: 'E',
-      zipcode: '05'
+      zipCode: '05'
     },
     {
-      firstname: 'TestEmployee5',
-      lastname: 'Five',
-      startdate: '05/12/2022',
+      firstName: 'TestEmployee5',
+      lastName: 'Five',
+      startDate: '05/12/2022',
       department: 'Legal',
       dob: '10/09/1999',
       street: 'Fstreet',
       city: 'Ecity',
       state: 'F',
-      zipcode: '06'
+      zipCode: '06'
     }
   ];
   
